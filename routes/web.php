8c063a51ccc0b06d72c9ea5839ecd52e9a7875f1
unknown <nazwanaz23456@gmail.com>
2025-02-10 11:19:56 +0700

    Second

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/admin/inputbuku', function () {
+    return view('admin.inputbuku');
+});
+
+// Route::get('/admin/inputkategori', function () {
+//     return view('admin.inputkategori');
+// });
+
+Route::get('/admin/inputkategori', [CategoryController::class, 'Index'])
+                                    ->name("admin.inputkategori");
+
+Route::get('/admin/inputpenulis', function () {
+    return view('admin.inputpenulis');
 });
